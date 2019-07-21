11057225dffaacb14631c6aba99223fff4f36b0b
mattHawthorn <hawthorn.matthew@gmail.com>
2019-07-21 15:42:08 -0400

    Bump version: 0.1.0 → 0.1.1

--- bourbaki/regex/__init__.py
+++ bourbaki/regex/__init__.py
@@ -4,4 +4,4 @@ from .base import CharRange, CharSet, CharClass, C, L, Literal, If, Alternation
 from .base import START, END, ANYCHAR, StartString, EndString, Tab, Endline, BackSpace, CarriageReturn
 from .base import WordBoundary, WordInternal, WordChar, NonWordChar, Digit, NonDigit, Whitespace, NonWhitespace
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
